068a64a35c94316b4269b4bf28da69ccc8d23e48
Matthaios Ch. <34523286+ChMatthaios@users.noreply.github.com>
2026-09-07 22:33:46 +0300

    Add SPARQL OPTIONAL UNION regression input

---
+++ tests/41_sparql_optional_union.sql
@@ -0,0 +1,1 @@
+PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT DISTINCT ?person ?label WHERE { ?person a foaf:Person . OPTIONAL { ?person foaf:name ?label . } { ?person foaf:knows ?friend . } UNION { ?person foaf:member ?friend . } } ORDER BY LCASE(?label) OFFSET 10 LIMIT 20
