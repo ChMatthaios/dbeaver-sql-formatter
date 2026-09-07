471b7bc045beeee18692d9c7f404f390e8e98d89
Matthaios Ch. <34523286+ChMatthaios@users.noreply.github.com>
2026-09-07 21:56:19 +0300

    Add PL/SQL anonymous block expected output

---
+++ tests_out/38_oracle_anonymous_block.out.sql
@@ -0,0 +1,15 @@
+DECLARE
+  v_customer_id NUMBER := 1001;
+  v_name VARCHAR2(100);
+  BEGIN
+    SELECT CUSTOMER_ID,
+           CUSTOMER_NAME
+      INTO v_customer_id, v_name
+      FROM CUSTOMER
+     WHERE CUSTOMER_ID = v_customer_id;
+    DBMS_OUTPUT.PUT_LINE(q'[Customer: ]' || v_name);
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    v_name := NULL;
+  END;
+/
